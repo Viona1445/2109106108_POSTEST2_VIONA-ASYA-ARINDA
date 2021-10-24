401e5479e24961c6100c07240e2b3827a516ace4
Viona1445 <93033602+Viona1445@users.noreply.github.com>
2021-10-24 18:01:19 +0800

    Add files via upload

---
+++ postest 2.2 .py
@@ -0,0 +1,12 @@
+#list
+biodata = ["Viona Asya Arinda", 2109106108, "Informatika", "B'21", "1.60 m"]
+print (biodata)
+
+#dictionary
+biodata ={
+        "Nama"          : "Viona Asya Arinda",
+        "NIM"           : 2109106108,
+        "Prodi"         : "Informatika",
+        "Kelas"         : "B'21",
+        "Tinggi Badan"  : "1.60 m"}
+print(biodata)
